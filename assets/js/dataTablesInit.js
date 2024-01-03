1ac494ff75c374993a2c4d154f78f308de6b60d5
antonioiivm99 <antoniovm99@javerianacali.edu.co>
2024-01-03 13:58:53 -0500

    Primer commit

---
+++ assets/js/dataTablesInit.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#miTabla').bootstrapTable({
+        responsive: true,// Para habilitar la responsividad
+        pagination: true,
+        search: true
+    });
+});
+
+$(document).ready(function() {
+    $('#miTabla2').bootstrapTable({
+        responsive: true,// Para habilitar la responsividad
+        pagination: true,
+        search: true
+    });
+});
+
+
+$(document).ready(function() {
+    $('#miTabla3').bootstrapTable({
+        responsive: true,// Para habilitar la responsividad
+        pagination: true,
+        search: true
+    });
+});
